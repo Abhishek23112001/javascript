e8065cf4202bb4838b81fad6075a274ddb094a49
Abhishek23112001 <130153682+Abhishek23112001@users.noreply.github.com>
2024-12-26 23:51:59 +0530

    Add files via upload

---
+++ class-1/main.js
@@ -0,0 +1,166 @@
+// console.log("hello world");
+
+// let firstName= "harkirat" ;
+// let isMarried= true;
+
+
+// if(isMarried==false){
+// console.log(firstName +" is not married");
+// }
+// else{
+//     console.log(firstName + "is married")}
+
+
+
+
+    // let b=0;
+    // for(let i=0; i<=1000; i++){
+    //     b=b + i;
+       
+    // }
+    // console.log(b);
+
+
+
+// const ages= [21, 22, 23, 24, 25];
+
+// for(let i=0; i<ages.length; i++){
+//     if(ages[i] % 2 ==0){
+//         console.log(ages[i]);
+//     }
+
+// }
+
+
+// by arrar
+// const personArray= ["harkirat", "raman", "priya"];
+// const genderArray=["male", "male"];
+
+
+//by object
+// const users1={
+//     firstName:"harkirat",
+//     gender:"male"
+// }
+// console.log(users1["gender"]);
+
+
+
+// const numberOfUsers=personArray.length;
+// for(let i=0; i<numberOfUsers; i++){
+//     if(genderArray[i]=="male"){
+//         console.log(personArray[i]);
+//     }
+// }
+
+
+
+//by array of object
+
+// const allUsers=[{
+//     firstName:"harkirat",
+//     gender:"male",
+//     metadata: {
+//         age:22,
+//         address:""
+//     }
+// },
+// {
+//     firstName:"raman",
+//     gender:"male",
+//     metadata: {
+//         age:21,
+//         address:""
+//     }
+// },
+// {
+//     firstName:"priya",
+//     gender:"female",
+//     metadata: {
+//         age:24,
+//         address:""
+//     }
+// }
+// ]
+
+// for(let i=0; i<allUsers.length; i++){
+//     if(allUsers[i].gender=="male"){
+//         console.log(allUsers[i])
+//     }
+// }  
+
+
+
+// for(let i=0; i<allUsers.length; i++){
+//     if(allUsers[i].metadata.age=== 21){
+//         console.log(allUsers[i].firstName);
+//     }
+// }
+    
+
+
+//function 
+
+// function sum(a,b){
+//     const sumValue=a+b;
+//     return sumValue;
+// }
+// const ans=sum(2,3);
+// console.log(ans); 
+
+
+//callback function
+
+// function sum(num1, num2){
+//     let result= num1+num2;
+//     // return result;
+//     displayResult(result);
+// }
+
+// function displayResult(data){
+//     console.log("result of the sum is: " +data)
+// }
+
+// function displayResultPassive(data){
+//     console.log("sum result is : " + data)
+// }
+
+// const ans= sum(1,2);
+
+
+
+// function sum(num1, num2, callback){
+//     let result= num1+num2;
+//       callback(result);
+   
+// }
+
+// function displayResult(data){
+//     console.log("result of the sum is: " +data)
+// }
+
+// function displayResultPassive(data){
+//     console.log("sum result is : " + data)
+// }
+
+// const ans= sum(1,2, displayResult);
+
+
+ 
+// set time out function
+
+// function greet(){
+//     console.log("hello world");
+// }
+
+// function greetAlien(){
+//     console.log('hello alien');
+// }
+// setTimeout(greetAlien, 5000)
+
+// setInterval(greetAlien, 2*1000);
+
+
+
+
+ 
